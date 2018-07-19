ffd5a0553423fd66dc82f58b144a98419f8b25a2
hiramcastrod <hiramc@live.com.mx>
2018-07-18 23:31:34 -0600

    add intents, navigation drawer

--- app/src/main/java/hiram/lavendimia/activities/NewClientActivity.java
+++ app/src/main/java/hiram/lavendimia/activities/NewClientActivity.java
@@ -2,14 +2,26 @@ package hiram.lavendimia.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import hiram.lavendimia.R;
 
 public class NewClientActivity extends AppCompatActivity {
-
+    EditText etName, etLastname, etRFC;
+    Button btAccept, btCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_client);
+        etName = findViewById(R.id.edittext_name);
+        etLastname = findViewById(R.id.edittext_lastname);
+        etRFC = findViewById(R.id.edittext_rfc);
+        btAccept = findViewById(R.id.button_accept);
+        btCancel = findViewById(R.id.button_cancel);
+    }
+
+    public void registerClient(){
+
     }
 }
