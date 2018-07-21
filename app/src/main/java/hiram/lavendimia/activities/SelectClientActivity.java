4d893f50af69ac8df11e1558bf8cb760b3a93bff
hiramcastrod <hiramc@live.com.mx>
2018-07-21 14:20:34 -0600

    pop up select client view, notebook base notes xd

--- app/src/main/java/hiram/lavendimia/activities/SelectClientActivity.java
+++ app/src/main/java/hiram/lavendimia/activities/SelectClientActivity.java
@@ -2,14 +2,40 @@ package hiram.lavendimia.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import hiram.lavendimia.R;
 
 public class SelectClientActivity extends AppCompatActivity {
+    private Spinner spinSelectClient;
+    Button btAcceptSelectClient, btCancelSelectClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_client);
+        spinSelectClient = findViewById(R.id.spinner_clients);
+        btAcceptSelectClient = findViewById(R.id.button_accept_selectclient);
+        btCancelSelectClient = findViewById(R.id.button_cance_selectclient);
+
+        DisplayMetrics displayMetrics =  new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        getWindow().setLayout((int) (width*.6), (int) (height*.6));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
     }
 }
--- app/src/main/java/hiram/lavendimia/fragments/SalesFragment.java
+++ app/src/main/java/hiram/lavendimia/fragments/SalesFragment.java
@@ -1,19 +1,19 @@
 package hiram.lavendimia.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import hiram.lavendimia.R;
+import hiram.lavendimia.activities.SelectClientActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SalesFragment extends Fragment {
-
+    FloatingActionButton fabNewSale;
 
     public SalesFragment() {
         // Required empty public constructor
@@ -24,7 +24,18 @@ public class SalesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sales, container, false);
+        View view =  inflater.inflate(R.layout.fragment_sales, container, false);
+        fabNewSale = view.findViewById(R.id.fab_add_sale);
+
+        fabNewSale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SelectClientActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
 }
